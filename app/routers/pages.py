7538cffdff6fb9e0f75e294ae283885e9d7b3207
Ruben Corro Blanco <rubencorrob@gmail.com>
2026-06-04 17:22:11 -0500

    Permite verificaciones HEAD en pagina principal

--- app/routers/pages.py
+++ app/routers/pages.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Request
+from fastapi import Response
 from fastapi.responses import FileResponse
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -14,6 +15,11 @@ def home(request: Request) -> HTMLResponse:
     return templates.TemplateResponse(
         request, "index.html", {"app_name": settings.app_name}
     )
+
+
+@router.head("/", include_in_schema=False)
+def home_head() -> Response:
+    return Response(status_code=200)
 
 
 @router.get("/favicon.ico", include_in_schema=False)
